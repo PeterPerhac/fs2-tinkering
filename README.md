fc4b50808e2bcccdb993b97663f1b0a407bbc735
Peter Perhac <perhac.peter@gmail.com>
2018-10-26 16:12:59 +0100

    fixing README

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Look ma - I'm streaming functionally!
 
-started reading the (http://fs2.io/guide.html)[FS2 guide] on: 26-Oct-2018
+started reading the [FS2 guide](http://fs2.io/guide.html) on: 26-Oct-2018
 
--- src/main/scala/com/perhac/experiments/fs2/Main.scala
+++ src/main/scala/com/perhac/experiments/fs2/Main.scala
@@ -1,6 +1,17 @@
 package com.perhac.experiments.fs2
 
+import cats.effect.IO
+import fs2.Stream
+
 object Main extends App {
   println("Look ma! I stream functionally!")
+  Stream.emit(1).repeat.take(10).toList.foreach(println)
+
+  val effectfulStream = Stream.eval(IO {
+    println("BEING RUN!!")
+    1 + 1
+  })
+  effectfulStream.compile.toList.unsafeRunSync().foreach(println)
+
 }
 
